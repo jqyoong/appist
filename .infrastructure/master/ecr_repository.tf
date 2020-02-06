35c2823eebef16d57e950b1e33d8153bfc129515
cayter <cayter1986@gmail.com>
2020-02-07 01:24:50 +0800

    Allow ECR image pull in all AWS accounts

--- .infrastructure/master/ecr_repository.tf
+++ .infrastructure/master/ecr_repository.tf
@@ -6,3 +6,25 @@ resource "aws_ecr_repository" "appist" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository_policy" "appist" {
+  repository = aws_ecr_repository.appist.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPull",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability"
+            ]
+        }
+    ]
+}
+EOF
+}
